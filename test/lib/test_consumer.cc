dc82edbef8a5c86130f6dafb11414e933d27d77f
Blake Smith <blakesmith0@gmail.com>
2015-06-03 21:29:20 -0500

    Add additional consumer delay test

--- test/lib/test_consumer.cc
+++ test/lib/test_consumer.cc
@@ -39,6 +39,19 @@ static ledger_consume_status consume_function(ledger_consumer_ctx *ctx, ledger_m
     return LEDGER_CONSUMER_OK;
 }
 
+static ledger_consume_status concat_consume_function(ledger_consumer_ctx *ctx, ledger_message_set *messages, void *data) {
+    std::string *concated_str = static_cast<std::string*>(data);
+    int i;
+
+    for(i = 0; i < messages->nmessages; i++) {
+        std::string new_message((const char *)messages->messages[i].data,
+                                messages->messages[i].len);
+        *concated_str = *concated_str + new_message;
+    }
+
+    return LEDGER_CONSUMER_OK;
+}
+
 TEST(LedgerConsumer, ConsumingSinglePartitionNoThreading) {
     ledger_ctx ctx;
     ledger_topic_options options;
@@ -60,12 +73,45 @@ TEST(LedgerConsumer, ConsumingSinglePartitionNoThreading) {
     consumer_opts.read_chunk_size = 1;
     ASSERT_EQ(LEDGER_OK, ledger_consumer_init(&consumer, consume_function, &consumer_opts, &consumed_size));
     ASSERT_EQ(LEDGER_OK, ledger_consumer_attach(&consumer, &ctx, TOPIC, 0));
-    
     EXPECT_EQ(LEDGER_OK, ledger_consumer_start(&consumer, 0));
 
     ledger_consumer_wait_for_position(&consumer, status.message_id);
     ledger_consumer_stop(&consumer);
     EXPECT_EQ(10, consumed_size);
+
+    ledger_consumer_close(&consumer);
+    ledger_close_context(&ctx);
+    ASSERT_EQ(0, cleanup(WORKING_DIR));
+}
+
+TEST(LedgerConsumer, ConsumingMessagesWithDelay) {
+    ledger_ctx ctx;
+    ledger_topic_options options;
+    ledger_consumer consumer;
+    ledger_consumer_options consumer_opts;
+    ledger_write_status status;
+
+    cleanup(WORKING_DIR);
+    ASSERT_EQ(0, setup(WORKING_DIR));
+    ASSERT_EQ(LEDGER_OK, ledger_open_context(&ctx, WORKING_DIR));
+    ASSERT_EQ(LEDGER_OK, ledger_topic_options_init(&options));
+    ASSERT_EQ(LEDGER_OK, ledger_open_topic(&ctx, TOPIC, 1, &options));
+
+    std::string concated_str;
+    ASSERT_EQ(LEDGER_OK, ledger_init_consumer_options(&consumer_opts));
+    consumer_opts.read_chunk_size = 2;
+    ASSERT_EQ(LEDGER_OK, ledger_consumer_init(&consumer, concat_consume_function, &consumer_opts, &concated_str));
+    ASSERT_EQ(LEDGER_OK, ledger_consumer_attach(&consumer, &ctx, TOPIC, 0));
+    EXPECT_EQ(LEDGER_OK, ledger_consumer_start(&consumer, 0));
+
+    ASSERT_EQ(LEDGER_OK, ledger_write_partition(&ctx, TOPIC, 0, (void *)"hello", 5, &status));
+    ledger_consumer_wait_for_position(&consumer, status.message_id);
+
+    ASSERT_EQ(LEDGER_OK, ledger_write_partition(&ctx, TOPIC, 0, (void *)"there", 5, &status));
+
+    ledger_consumer_wait_for_position(&consumer, status.message_id);
+    ledger_consumer_stop(&consumer);
+    EXPECT_EQ("hellothere", concated_str);
 
     ledger_consumer_close(&consumer);
     ledger_close_context(&ctx);
