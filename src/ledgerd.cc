b903b656e6d638501d34255ede7e0eda6b0129d6
Blake Smith <blakesmith0@gmail.com>
2015-06-26 20:24:34 -0500

    Wire up the actual server implementation.

--- src/ledgerd.cc
+++ src/ledgerd.cc
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include <iostream>
+#include <memory>
+
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_credentials.h>
+
+#include "server.h"
+
+using namespace ledgerd;
 
 int main() {
-    printf("Hello world!\n");
+    std::string server_address("0.0.0.0:50051");
+    LedgerdServiceImpl service;
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+    server->Wait();
 
     return 0;
 }
