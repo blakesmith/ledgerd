7dc44af7fd7f74f2f872586a7dc1b18855580340
Blake Smith <blakesmith0@gmail.com>
2015-07-11 11:48:36 -0500

    Add initial grpc interface ping test

---
+++ test/test_grpc_interface.cc
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+
+#include <memory>
+#include <thread>
+
+#include <grpc++/server.h>
+#include <grpc++/server_builder.h>
+#include <grpc++/server_credentials.h>
+#include <grpc++/channel_arguments.h>
+#include <grpc++/credentials.h>
+#include <grpc++/create_channel.h>
+
+#include "client.h"
+#include "ledgerd_service.h"
+#include "ledgerd_service_config.h"
+#include "grpc_interface.h"
+
+namespace ledger_grpc_interface_test {
+
+using namespace ledgerd;
+
+static LedgerdClient build_client() {
+    LedgerdClient client(
+        grpc::CreateChannel("localhost:50051", grpc::InsecureCredentials(),
+                           grpc::ChannelArguments()));
+    return std::move(client);
+}
+
+TEST(GrpcInterface, PingPong) {
+    LedgerdServiceConfig config;
+    config.set_grpc_address("0.0.0.0:50051");
+    config.set_root_directory("/tmp/ledgerd");
+    LedgerdService ledgerd_service(config);
+    GrpcInterface grpc_interface(ledgerd_service);;
+    grpc::ServerBuilder builder;
+    builder.AddListeningPort(config.get_grpc_address(), grpc::InsecureServerCredentials());
+    builder.RegisterService(&grpc_interface);
+    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+    LedgerdClient client = build_client();
+
+    std::string pong = client.Ping();
+    EXPECT_EQ("pong", pong);
+
+    server->Shutdown();
+}
+
+}
