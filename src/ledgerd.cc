29009dfdb6102c6ac1837ad46520b5ead582c397
Blake Smith <blakesmith0@gmail.com>
2016-01-13 22:03:45 -0600

    Start and Stop the cluster manager

--- src/ledgerd.cc
+++ src/ledgerd.cc
@@ -17,11 +17,16 @@ int main(int argc, char **argv) {
     LedgerdServiceConfig config;
     config.set_grpc_address("0.0.0.0:64399");
     config.set_root_directory("/tmp/ledgerd");
+    config.set_grpc_cluster_address("0.0.0.0:64398");
+    config.set_cluster_node_id(0);
+    config.add_node(0, "0.0.0.0:64338");
+
     LedgerdService ledgerd_service(config);
     ClusterManager cluster_manager(config.cluster_node_id(),
                                    ledgerd_service,
                                    config.grpc_cluster_address(),
                                    config.node_info());
+    cluster_manager.Start();
     GrpcInterface grpc_interface(ledgerd_service, cluster_manager);
     grpc::ServerBuilder builder;
     builder.AddListeningPort(config.get_grpc_address(), grpc::InsecureServerCredentials());
@@ -29,6 +34,7 @@ int main(int argc, char **argv) {
     std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << config.get_grpc_address() << std::endl;
     server->Wait();
+    cluster_manager.Stop();
 
     return 0;
 }
