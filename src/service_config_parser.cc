ca00cf2ca21afbeba7f044eae087cf1fe7aa361f
Blake Smith <blakesmith0@gmail.com>
2016-02-20 16:12:09 -0600

    Add remaining service config parser options

--- src/service_config_parser.cc
+++ src/service_config_parser.cc
@@ -12,15 +12,36 @@ ServiceConfigParser::ServiceConfigParser() {
 std::unique_ptr<LedgerdServiceConfig> ServiceConfigParser::MakeServiceConfig(int argc, char **argv) {
     static struct option longopts[] = {
         { "root", required_argument, 0, 'r' },
+        { "grpc-address", required_argument, 0, 'g' },
+        { "cluster-address", required_argument, 0, 'c' },
+        { "cluster-node-id", required_argument, 0, 'i' },
+        { "default-partition-count", required_argument, 0, 'p' },
+        { "peer-nodes", required_argument, 0, 'n' },
         { 0, 0, 0, 0}
     };
     int ch;
     std::unique_ptr<LedgerdServiceConfig> config = std::unique_ptr<LedgerdServiceConfig>(new LedgerdServiceConfig());
 
-    while((ch = getopt_long(argc, argv, "r:", longopts, NULL)) != -1) {
+    while((ch = getopt_long(argc, argv, "r:g:c:i:n:", longopts, NULL)) != -1) {
         switch(ch) {
             case 'r':
                 config->set_root_directory(std::string(optarg, strlen(optarg)));
+                break;
+            case 'g':
+                config->set_grpc_address(std::string(optarg, strlen(optarg)));
+                break;
+            case 'c':
+                config->set_grpc_cluster_address(std::string(optarg, strlen(optarg)));
+                break;
+            case 'i':
+                config->set_cluster_node_id(atoi(optarg));
+                break;
+            case 'p':
+                config->set_default_partition_count(atoi(optarg));
+                break;
+            case 'n':
+                config->add_node(0, "blah:blah");
+                config->add_node(1, "blah:blah");
                 break;
             default:
                 break;
--- test/test_service_config_parser.cc
+++ test/test_service_config_parser.cc
@@ -9,12 +9,22 @@ using namespace ledgerd;
 TEST(ServiceConfigParser, AllOptions) {
     ServiceConfigParser parser;
     const char *argv[] { "ledgerd",
-            "--root", "/tmp/ledgerd"};
+            "--root", "/tmp/ledgerd",
+            "--grpc-address", "0.0.0.0:643",
+            "--cluster-address", "0.0.0.0:645",
+            "--peer-nodes", "{0=>0.0.0.0:645,1=>0.0.0.0:645}",
+            "--cluster-node-id", "2",
+            "--default-partition-count", "5"};
     int argc = sizeof(argv) / sizeof(char*);
     
     auto config = parser.MakeServiceConfig(argc, const_cast<char**>(argv));
     ASSERT_TRUE(config != nullptr);
     EXPECT_EQ("/tmp/ledgerd", config->get_root_directory());
+    EXPECT_EQ("0.0.0.0:643", config->get_grpc_address());
+    EXPECT_EQ("0.0.0.0:645", config->grpc_cluster_address());
+    EXPECT_EQ(2, config->cluster_node_id());
+    EXPECT_EQ(5, config->default_partition_count());
+    EXPECT_EQ(2, config->node_info().size());
 }
 
 }
